f4d2f9fe0d6615873a61ad433b7bed89bf18a21a
Skellzz <george.teemus@gmail.com>
2025-10-30 11:29:37 +0200

    Lisasin models movie kausta ActorsViewModel

--- Filminurk/Filminurk/Controllers/ActorsController.cs
+++ Filminurk/Filminurk/Controllers/ActorsController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Filminurk.Core.ServiceInterface;
+using Filminurk.Data;
+using Filminurk.Models.Movies;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Filminurk.Controllers
 {
     public class ActorsController : Controller
     {
-        public IActionResult Index()
+        private readonly FilminurkTARpe24Context _context;
+        private readonly IMovieServices _movieServices;
+        private readonly IFilesServices _filesServices;
+
+        public ActorsController
+            (
+                FilminurkTARpe24Context context,
+                IMovieServices movieServices,
+                IFilesServices filesServices
+
+            )
         {
-            return View();
+            _context = context;
+            _movieServices = movieServices;
+            _filesServices = filesServices;
         }
+    }
+    public IActionResult Index()
+        {
+            var result = _context.Movies.Select(x => new MoviesIndexViewModel
+            {
+                ID = x.ID,
+                Title = x.Title,
+                FirstPublished = x.FirstPublished,
+                CurrentRating = x.CurrentRating,
+                CountryOfOrigin = x.CountryOfOrigin,
+                MovieGenre = x.MovieGenre,
+
+            });
+            return View(result);
+        }
+
     }
 }
