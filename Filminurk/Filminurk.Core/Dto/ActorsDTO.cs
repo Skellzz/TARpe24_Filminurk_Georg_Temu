392c90c9371c14392e8918e5ffe5080067e45531
Skellzz <george.teemus@gmail.com>
2025-10-30 10:01:33 +0200

    Tegin DTO folderisse ActorsDTO ja lisasin andmed

--- Filminurk/Filminurk.Core/Dto/ActorsDTO.cs
+++ Filminurk/Filminurk.Core/Dto/ActorsDTO.cs
@@ -6,15 +6,30 @@ using System.Threading.Tasks;
 
 namespace Filminurk.Core.Dto
 {
+    public enum ParimFilm
+    {
+        VelecoPastor,
+        Dursticpark,
+        Tuhkatriinu,
+        Matrix,
+        Inception,
+        Cars3,
+        Oilmaailm
+
+    }
     public class ActorsDTO
     {
-      public Guid ActorID { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Nickname { get; set; }
         public List<string> MoviesActedFor { get; set; }
         public int? PortraitID { get; set; }
 
-        //enda tehhtud andmed
+        // Enda loodud andmed
 
+        public string? Description { get; set; }
+        public int? Rating { get; set; }
+        public ParimFilm? ParimFilm { get; set; }
     }
 }
