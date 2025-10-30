a7ae32d7be99baffe43cb3d132f80d5ebdd7ba29
Skellzz <george.teemus@gmail.com>
2025-10-30 10:51:14 +0200

    lisasin Domaini uue classi Actor

---
+++ Filminurk/Filminurk.Core/Domain/Actor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public enum ParimFilm
+    {
+        VelecoPastor,
+        Dursticpark,
+        Tuhkatriinu,
+        Matrix,
+        Inception,
+        Cars3,
+        Oilmaailm
+
+    }
+    public class Actor
+    {
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Nickname { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public int? PortraitID { get; set; }
+
+        // Enda loodud andmed
+
+        public string? Description { get; set; }
+        public int? Rating { get; set; }
+        public ParimFilm? ParimFilm { get; set; }
+    }
+}
