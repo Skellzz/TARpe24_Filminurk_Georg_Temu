9980a316aca70dc8b5159b3622fa2f6e8bc53b33
Opilane TTHK <opilane@tthk.ee>
2025-12-05 12:20:10 +0200

    k

--- Filminurk/Filminurk.Data/EnviromentExample.cs
+++ Filminurk/Filminurk.Data/EnviromentExample.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Filminurk.Data
 {
-    public class Enviroment
+    public class EnviromentExample //reanme class to just "Enviorment"
     {
+        public static string smtppaddress = "";//server address for sending emails via smtp
+        public static string gmailiapppassword = ""; //password for user, taht has access to this server (google app password)
+        public static string gmailusername = "";//username of email sending user (gmail account)
     }
 }
