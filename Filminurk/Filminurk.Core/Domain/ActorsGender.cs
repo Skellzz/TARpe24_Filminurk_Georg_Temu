6d5c244dcd37c800304274c4617c4cb54f424a58
Opilane TTHK <>
2026-01-15 15:04:32 +0200

    Lisasin Domain folderisse Actors.cs ja ActorsGender.cs

---
+++ Filminurk/Filminurk.Core/Domain/ActorsGender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//ActorGenderi koht lelelelele
+namespace Filminurk.Core.Domain
+{
+    public enum Gender
+    {
+        Male, Female, WalmartBag// <- vana internetti meme
+    }
+}
