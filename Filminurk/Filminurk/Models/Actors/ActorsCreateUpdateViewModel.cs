cdd0495aee8789226846f57f62eb2e759666b372
Opilane TTHK <>
2026-01-15 15:19:34 +0200

    Tegin Models kasuta Actorsi kausta ja sinna sisse ActorsCreateUpdateViewModel, ActorsDeleteViewModel, ActorsDetailsViewModel, ActorsIndexViewModel

---
+++ Filminurk/Filminurk/Models/Actors/ActorsCreateUpdateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Filminurk.Core.Domain;
+using Filminurk.Core.Dto;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsCreateUpdateViewModel
+    {
+        public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public string? PortraitID { get; set; }
+        public List<IFormFile>? Files { get; set; }
+        public IEnumerable<FileToApiDTO>? Images { get; set; } = new List<FileToApiDTO>();
+
+
+        public int? ActorRating { get; set; }
+        public Gender? Gender { get; set; }
+        public Genre? MostActedGenre { get; set; }
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
---
+++ Filminurk/Filminurk/Models/Actors/ActorsIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Filminurk.Core.Domain;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsIndexViewModel
+    {
+        public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? NickName { get; set; }
+
+
+        public int? ActorRating { get; set; }
+        public Gender? Gender { get; set; }
+        public Genre? MostActedGenre { get; set; }
+    }
+}
