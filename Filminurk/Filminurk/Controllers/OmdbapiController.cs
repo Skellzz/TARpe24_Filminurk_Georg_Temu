09af2f6f8efe72edcbc11853d61815b8989056c5
Skellzz <george.teemus@gmail.com>
2026-01-25 19:38:51 +0200

    Armastan bändi keskkool also ma parandasin kontrollerit

--- Filminurk/Filminurk/Controllers/OmdbapiController.cs
+++ Filminurk/Filminurk/Controllers/OmdbapiController.cs
@@ -16,92 +16,40 @@ using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json.Linq;
 using System;
 
+
+
 namespace Filminurk.Controllers
 {
     public class OmdbapiController : Controller
     {
         private readonly IOMDbApiServices _omdbapiServices;
+
         public OmdbapiController(IOMDbApiServices omdbapiServices)
         {
             _omdbapiServices = omdbapiServices;
         }
-        public IActionResult Index()
-        {
-            return View();
-        }
-        public IActionResult FindMovie(OMDbApiSearchViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                return Import(model.Title);
-            }
-            return NotFound();
-        }
 
         [HttpGet]
-        public IActionResult Import(string title)
+        public IActionResult Index()
         {
-            OMDbApiMovieResultDTO dto = new();
-            dto.Title = title;
-            _omdbapiServices.OMDbApiResult(dto);
-            OMDbApiViewModel vm = new();
-            vm.Title = dto.Title;
-            vm.FirstPublished = dto.FirstPublished;
-            if (Genre.IsDefined(typeof(Genre), dto.Genre))
-            {
-                vm.Genre = (Genre)Enum.Parse(typeof(Genre), dto.Genre);
-            }
-            else
-            {
-                vm.Genre = Genre.smthElse;
-            }
-            vm.imdbRating = dto.imdbRating;
-            vm.Actors = dto.Actors;
-            vm.Producer = dto.Director;
-            vm.Description = dto.Description;
-            return View("Import", vm);
+            return View(new OMDbApiSearchViewModel());
         }
+
         [HttpPost]
-        public IActionResult Import(OMDbApiViewModel vm)
+        public async Task<IActionResult> FindMovie(OMDbApiSearchViewModel model, CancellationToken ct)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
+                return View("Index", model);
+
+            var createdId = await _omdbapiServices.ImportByTitleAsync(model.Title, ct);
+
+            if (createdId == null)
             {
-                var dto = new OMDbApiMovieCreateDTO()
-                {
-                    ID = Guid.NewGuid(),
-                    Title = vm.Title,
-                    Description = vm.Description,
-                    Producer = vm.Producer,
-                    Actors = vm.Actors.Split(new char[] { ',' }).ToList(),
-                    CurrentRating = double.Parse(vm.imdbRating, System.Globalization.CultureInfo.InvariantCulture),
-                    Genre = vm.Genre,
-                    EntryCreatedAt = DateTime.Now,
-                    EntryModifiedAt = DateTime.Now,
-                };
-
-                if (vm.FirstPublished == "N/A" || vm.FirstPublished == null)
-                {
-                    dto.FirstPublished = DateOnly.MinValue;
-                }
-                else
-                {
-                    dto.FirstPublished = DateOnly.Parse(vm.FirstPublished);
-                }
-
-
-                var result = _omdbapiServices.Create(dto);
-                if (result == null)
-                {
-                    return NotFound();
-                }
-                if (!ModelState.IsValid)
-                {
-                    return NotFound();
-                }
+                ModelState.AddModelError("", "Filmi ei leitud OMDb-st või import ebaõnnestus.");
+                return View("Index", model);
             }
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Details", "Movies", new { id = createdId.Value });
         }
     }
-
 }
