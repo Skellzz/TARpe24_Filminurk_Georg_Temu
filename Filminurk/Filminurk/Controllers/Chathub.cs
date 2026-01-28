39034265d602eaa6a0ad01abebf1c9a764c63cf3
Skellzz <george.teemus@gmail.com>
2026-01-28 20:48:28 +0200

    vanille ekstrakt e tegin veel asju Program.cs ja ChatHub-is

--- Filminurk/Filminurk/Controllers/Chathub.cs
+++ Filminurk/Filminurk/Controllers/Chathub.cs
@@ -1,6 +1,12 @@
-﻿namespace Filminurk.Controllers
+﻿using System.Collections.Concurrent;
+using Filminurk.Models.Accounts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Filminurk.Controllers
 {
-    public class Chathub
+    public class ChatHub : Hub
     {
     }
 }
